f21da117cb730a39de5ae7f3ba9856da1afb0f92
Michael Pchelnikov <m.pchelnikov@gmail.com>
2018-07-31 22:52:25 +0300

    Project initialization

---
+++ PluggableAppDelegate/PluggableAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PluggableAppDelegate.h
+//  PluggableAppDelegate
+//
+//  Created by Mikhail Pchelnikov on 31/07/2018.
+//  Copyright © 2018 Michael Pchelnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PluggableAppDelegate.
+FOUNDATION_EXPORT double PluggableAppDelegateVersionNumber;
+
+//! Project version string for PluggableAppDelegate.
+FOUNDATION_EXPORT const unsigned char PluggableAppDelegateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PluggableAppDelegate/PublicHeader.h>
+
+
