6441f3053acf31413cfd54ad64db1512dd5dcf91
mcblooder <tusur.rodion@gmail.com>
2020-12-25 04:51:05 +0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PluggableAppDelegate",
+    products: [
+        .library(
+            name: "PluggableAppDelegate",
+            targets: ["PluggableAppDelegate"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "PluggableAppDelegate",
+            dependencies: [],
+            path: "Sources")
+    ]
+)
